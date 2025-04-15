bccb9b3c762dca9386c6baf3f723524849b7840d
gustavodias <gustavou.dias@gmail.com>
2025-04-15 10:18:15 -0300

    add tabela escolas

--- educacao/context_processors.py
+++ educacao/context_processors.py
@@ -1,8 +1,9 @@
 from .models import Turmas, Alunos, Nivel_Ensino
 from django.contrib.auth.decorators import login_required
 
-@login_required
-def get_turmas(request):    
+def get_turmas(request): 
+    if not request.user.is_authenticated:
+        return {}   
     turmas = Turmas.objects.filter(professor__user=request.user)
     niveis_ensino = Nivel_Ensino.objects.filter(turmas__professor__user=request.user).distinct()
     return {'turmas': turmas, 'niveis_ensino': niveis_ensino}
