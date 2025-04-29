57a7ad389be0d3e1c83a7a0de9b91308bfde0c33
gustavodias <gustavou.dias@gmail.com>
2025-04-29 16:52:20 -0300

    fix

--- educacao/context_processors.py
+++ educacao/context_processors.py
@@ -2,33 +2,22 @@ from .models import Turmas, Alunos, Nivel_Ensino
 from django.contrib.auth.decorators import login_required
 from autenticacao.models import Pessoa
 
+# Se for professor, ele terá acesso a todas as turmas e niveis de ensino da escola que ele é professor
+# Se for diretor, ele terá acesso a todas as turmas e niveis de ensino da escola que ele é diretor
+# Se for assistente administrativo, ele terá acesso a todas as turmas e niveis de ensino de todas as escolas
+# Se for administrador, ele terá acesso a todas as turmas e niveis de ensino de todas as escolas
+
 def get_turmas(request): 
     if not request.user.is_authenticated:
         return {}   
     
-    pessoa = Pessoa.objects.get(user=request.user)
-    dict_niveis_ensino = []
-
-    if pessoa.tipo_conta == 'pro':
-        # Professor: Access to all levels and classes in their school
-        niveis_ensino = Nivel_Ensino.objects.filter(turmas__escola__turmas__professor=pessoa).distinct()
-        for nivel in niveis_ensino:
-            turmas = Turmas.objects.filter(escola__turmas__professor=pessoa, nivel_ensino=nivel).distinct()
-            dict_niveis_ensino.append({'nivel': nivel, 'turmas': turmas})
-
-    elif pessoa.tipo_conta == 'dir':
-        # Director: Access to all levels and classes in their school
-        niveis_ensino = Nivel_Ensino.objects.filter(turmas__escola__diretor=pessoa).distinct()
-        for nivel in niveis_ensino:
-            turmas = Turmas.objects.filter(escola__diretor=pessoa, nivel_ensino=nivel).distinct()
-            dict_niveis_ensino.append({'nivel': nivel, 'turmas': turmas})
-
-    elif pessoa.tipo_conta in ['ass', 'adm']:
-        # Administrative Assistant and Administrator: Access to all levels and classes in all schools
-        niveis_ensino = Nivel_Ensino.objects.all().distinct()
-        for nivel in niveis_ensino:
-            turmas = Turmas.objects.filter(nivel_ensino=nivel).distinct()
-            dict_niveis_ensino.append({'nivel': nivel, 'turmas': turmas})
-
-    return {'niveis_ensino': dict_niveis_ensino, 'pessoa': pessoa}
+    niveis_ensino = Nivel_Ensino.objects.filter(turmas__professor__user=request.user).distinct()
+    dict_niveis_ensino =[]
+    for nivel in niveis_ensino:
+        turmas_1 = Turmas.objects.filter(nivel_ensino=nivel, professor__user=request.user).distinct()
+        turmas_2 = Turmas.objects.filter(nivel_ensino=nivel, escola__diretor__user=request.user).distinct()
+        turmas = turmas_1 | turmas_2
+        turmas = turmas.distinct()
+        dict_niveis_ensino.append({'nivel': nivel, 'turmas': turmas})
+    return {'niveis_ensino': dict_niveis_ensino, 'pessoa': Pessoa.objects.get(user=request.user)}
 
