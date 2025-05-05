84b1725f392bb6e807cd0ee06dd4636a7c254eee
gustavodias <gustavou.dias@gmail.com>
2025-05-05 11:41:36 -0300

    att

--- educacao/views.py
+++ educacao/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect, get_object_or_404
-import matplotlib.pyplot as plt
 from io import BytesIO
 from django.http import HttpResponse
 from django.shortcuts import render
